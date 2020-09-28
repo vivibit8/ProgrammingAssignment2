4d9fad2bfa6065f2138ca46053f00505660a6c1e
vivibit8 <viviguaman@gmail.com>
2020-09-28 11:04:50 -0500

    Week 3 Programming exercise

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,35 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## This function creates a matrix that can cache its inverse
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinvmatrix <- function(inverse) m <<- inverse
+  getinvmatrix <- function() m
+  list(set = set,
+       get = get,
+       setinvmatrix = setinvmatrix,
+       getinvmatrix = getinvmatrix)
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## This fuction computes the inverse matrix, if the inverse has been calculated, then the function should retrieve the inverse from the cache
+  m <- x$getinvmatrix()
+  
+  if (!is.null(m)){
+    message("getting cached data")
+    return(m)
+    
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setinvmatrix(m)
+  m
 }
